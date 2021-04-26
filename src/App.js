8f76ce82991b846c8357d1b3887c676ce9a733a3
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-04-26 14:29:58 +0600

    new: added custom font 'Heebo'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import '@fontsource/heebo/400.css';
+import '@fontsource/heebo/500.css';
+import '@fontsource/heebo/700.css';
+import customizedTheme from 'theme/theme';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -9,7 +14,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <ChakraProvider theme={theme}>
+        <ChakraProvider theme={customizedTheme}>
           <Route path="/" component={Home} exact />
           <Route path="/about" component={About} exact />
         </ChakraProvider>
