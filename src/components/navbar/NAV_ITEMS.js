3e0398d09ad863669aec8539944fa0432f909cea
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-04-27 00:10:02 +0600

    new: added navbar

---
+++ src/components/navbar/NAV_ITEMS.js
@@ -0,0 +1,57 @@
+export const NAV_ITEMS = [
+  {
+    label: 'Home',
+    href: '#',
+  },
+  {
+    label: 'About',
+    children: [
+      {
+        label: 'About IEEE',
+        href: '#',
+      },
+      {
+        label: 'About region 10',
+        href: '#',
+      },
+      {
+        label: 'About Bangladesh section',
+        href: '#',
+      },
+      {
+        label: 'About KU student branch',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Activities',
+    children: [
+      {
+        label: 'Events',
+        href: '#',
+      },
+      {
+        label: 'News',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Members',
+    children: [
+      {
+        label: 'All members',
+        href: '#',
+      },
+      {
+        label: 'Member benefits',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Contact',
+    href: '#',
+  },
+];
