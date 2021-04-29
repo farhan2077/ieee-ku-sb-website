9ca50d34942c4fa966294804a739c8427fd16b19
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-04-29 16:21:53 +0600

    fix: updated layout container

--- src/components/layout/LayoutContainer.js
+++ src/components/layout/LayoutContainer.js
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/layout';
 export default function LayoutContainer({ children }) {
   return (
     <div>
-      <Box maxW="6xl" margin="0 auto">
+      <Box maxW="6xl" margin="0 auto" px="3">
         {children}
       </Box>
     </div>
