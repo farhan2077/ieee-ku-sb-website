3573ed68551205ac83050be5da5c8099e49e2262
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-05-01 16:58:53 +0600

    new: disable right clicking one image

--- src/components/layout/PageContainer.js
+++ src/components/layout/PageContainer.js
@@ -3,12 +3,14 @@ import Navbar from 'components/navbar/Navbar';
 import Footer from 'components/Footer';
 import { Box } from '@chakra-ui/layout';
 
+import imgStyle from 'components/layout/ImgStyle.module.css';
+
 export default function PageContainer({ children }) {
   return (
     <div>
       <Box minH="100vh" display="flex" flexDir="column">
         <Navbar />
-        <div>{children}</div>
+        <div style={imgStyle}>{children}</div>
         <Footer />
       </Box>
     </div>
