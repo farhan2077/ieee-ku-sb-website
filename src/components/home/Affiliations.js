36b0d1db6edf6e5125e1c50cca873d34b5d85d67
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-05-09 16:39:36 +0600

    fix: updated affiliated logo + alt

--- src/components/home/Affiliations.js
+++ src/components/home/Affiliations.js
@@ -3,14 +3,19 @@ import { Image, Box } from '@chakra-ui/react';
 
 import SectionHeader from 'components/styled-components/SectionHeader';
 
-import router from 'data/affiliations/images/router.jpg';
+import router from 'data/affiliations/images/router.webp';
 
 export default function Affiliations() {
   return (
     <div>
       <SectionHeader sectionHeaderText="affiliations" center />
       <Box align="center" mt="8">
-        <Image height="20" width="auto" src={router} alt="Router" />
+        <Image
+          height="20"
+          width="auto"
+          src={router}
+          alt="Router, the club of ECE discipline Khulna University"
+        />
       </Box>
     </div>
   );
